c13322542cbbab0a78592fcd9d3330298c465319
theeazyfit <theeazyfit@gmail.com>
2026-04-27 01:48:54 +0100

    Deploy Sanity Studio to hbee-designs.sanity.studio

---
+++ studio/sanity.cli.js
@@ -0,0 +1,12 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'o7fduvas',
+    dataset: 'production'
+  },
+  studioHost: 'hbee-designs',
+  deployment: {
+    appId: 'nvovfmpfnpa51dy87gcxv0vl'
+  }
+})
